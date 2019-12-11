bbc9acccb98b3737f3b3d5aa34cd6baafc46cee2
Iago Cavalcante <iagoangelimc@gmail.com>
2019-12-11 00:46:40 -0300

    :building_construction: Criando componentes
    * Componentes de gestos
    * Removendo logica de dentro do app.js

--- squash-hardcore/App.js
+++ squash-hardcore/App.js
@@ -4,108 +4,19 @@ import { PIXI } from 'expo-pixi';
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import GestureView from './src/GestureView';
+import Game from './Game';
 
 export default class App extends React.Component {
 
   state = { score: 0 };
-  personaX = 0;
-  personaY = 0;
-  personaWidth = 0;
-  screenWidth = 0;
-
-  onTap = () => {
-    if (this.game) {
-      this.game.board.onTap();
-    }
-  };
 
   onSwipe = dir => {
-    console.log(dir)
-    if(dir === 1 && this.personaX < this.screenWidth - this.personaWidth) {
-      this.personaX += 7;
-    }
-    else if(dir === 2 && this.personaX > 0) {
-      this.personaX -= 7;
-    }
+    this.game.changePersonaPosition(dir);
   };
+
   onContextCreate = async context => {
-    const Settings = {
-      initialSize: 10,
-      tileSize: 20,
-      hasGrid: true,
-      foodColor: 0x6f42c1,
-      snakeColor: 0x50b948,
-      backgroundColor: 0xecf0f1,
-    };
-
-    let score = 0;
-
-    const app = new PIXI.Application({ context });
-
-    const background = await PIXI.Sprite.from(require('./assets/sprites/tiledbackground.png'));
-    const ground = await PIXI.Sprite.from(require('./assets/sprites/sprite-sheet0.png'));
-    const ball = await PIXI.Sprite.from(require('./assets/sprites/ball-sheet0.png'));
-    const graphics = new PIXI.Graphics();
-
-    ball.anchor.set(0.5);
-    ball.width = 70
-    ball.height = 70
-    
-    this.screenWidth = app.renderer.width;
-    const screenWidthCentered = this.screenWidth / 2;
-    const screenHeight = app.renderer.height;
-    const screenHeightCentered = screenHeight / 2;
-    this.personaWidth = 150;
-    const personaHeight = 30;
-    const ballRadius = 10;
-    this.personaX = (this.screenWidth - this.personaWidth) / 2;
-    this.personaY = screenHeight - personaHeight;
-
-    ball.x = this.screenWidthCentered;
-    ball.y = screenHeightCentered;
-    background.width = this.screenWidth;
-    background.height = screenHeight;
-    ground.width = this.screenWidth;
-    ground.height = screenHeight;
-
-    const directions = {
-      x: 2,
-      y: -2
-    }
-
-    graphics.lineStyle(2, 0x0000ff, 1);
-    graphics.beginFill(0xff700b, 1);
-    graphics.drawRect(this.personaX, this.personaY, this.personaWidth, personaHeight);
-
-    app.stage.addChild(background);
-    app.stage.addChild(ground);
-    app.stage.addChild(graphics);
-    app.stage.addChild(ball);
-    app.ticker.add((delta) => {
-      if (ball.x > this.screenWidth - ballRadius || ball.x + directions.x < ballRadius) {
-        directions.x = - directions.x;
-      }
-      if (ball.y + directions.y < ballRadius) {
-        directions.y = - directions.y;
-      }
-      else if(ball.y > screenHeight - ballRadius) {
-        if(ball.x > this.personaX && ball.x < this.personaX + this.personaWidth) {
-          score += 1;
-          this.setState({ score });
-          directions.y = - directions.y;
-        }
-        else {
-          score = 0;
-          this.setState({ score });
-          app.ticker.stop()
-          app.ticker.start()
-        }
-      }
-      ball.y += directions.y;
-      ball.x += directions.x;
-      ball.rotation += 0.1 * delta;
-    });
-
+    this.game = new Game(context)
+    this.game.score = score => this.setState({ score });
   };
   render() {
     return (
